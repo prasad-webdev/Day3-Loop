5f69456a2ef14fbcd79317287a74b1c241f6d64e
Prasad Bairagi <prasadbairagi131@gmail.com>
2026-09-15 10:28:24 +0530

    updated

--- passw.py
+++ passw.py
@@ -1,18 +1,10 @@
-# correct_pass = "741852" 
-# not_found = True
+correct_pass = "741852" 
+not_found = True
 
-# while not_found:
-#     passw = input("enter passs: ")
-#     if passw == correct_pass:
-#         break
-#         # not_found = False
+while not_found:
+    passw = input("enter passs: ")
+    if passw == correct_pass:
+        break
+        # not_found = False
 
-# print("password match")
-
-i = 0
-while i < 10:
-    if i == 5:
-        i += 1
-        continue
-    print(i)
-    i += 1
+print("password match")
